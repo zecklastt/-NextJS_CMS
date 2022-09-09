013dda13b35309573bca14bae299895aa938add5
José Alfredo <jose.alfredo@veri.com.br>
2022-09-09 11:45:54 -0300

    Aula 3.1 again

--- src/components/commons/Footer/index.js
+++ src/components/commons/Footer/index.js
@@ -1,39 +1,41 @@
+import { getCMSContent } from '../../../infra/cms/CMSProvider';
 import { Box, Text, Link, theme } from '../../../theme/components';
 
-export function Footer({ description }) {
-  return (
-    <Box
-      tag="footer"
-      styleSheet={{
-        backgroundColor: theme.colors.neutral.x800,
-      }}
-    >
+export function Footer() {
+  const description = getCMSContent().globalContent?.globalFooter?.description;
+    return (
       <Box
+        tag="footer"
         styleSheet={{
-          overflow: "hidden",
-          maxWidth: theme.space.xcontainer_xl,
-          marginLeft: "auto",
-          marginRight: "auto",
-          paddingVertical: theme.space.x8,
-          paddingHorizontal: theme.space.x4,
+          backgroundColor: theme.colors.neutral.x800,
         }}
       >
-        <Text
+        <Box
           styleSheet={{
-            justifyContent: 'center',
-            color: theme.colors.neutral.x000
+            overflow: "hidden",
+            maxWidth: theme.space.xcontainer_xl,
+            marginLeft: "auto",
+            marginRight: "auto",
+            paddingVertical: theme.space.x8,
+            paddingHorizontal: theme.space.x4,
           }}
         >
-          &copy; {new Date().getFullYear()} {description}
-        </Text>
-        {
-          process.env.NODE_ENV !== 'production' && (
-            <Link href="/api/preview?password=SENHASEGURA">
-              Toggle Preview Mode
-            </Link>
-          )
-        }
+          <Text
+            styleSheet={{
+              justifyContent: 'center',
+              color: theme.colors.neutral.x000
+            }}
+          >
+            &copy; {new Date().getFullYear()} {description}
+          </Text>
+          {
+            process.env.NODE_ENV !== 'production' && (
+              <Link href="/api/preview?password=SENHASEGURA">
+                Toggle Preview Mode
+              </Link>
+            )
+          }
+        </Box>
       </Box>
-    </Box>
-  )
+    )
 }
