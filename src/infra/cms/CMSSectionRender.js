17a4fbe24bd1e841cf1dfb9665f9550bfe62b02b
José Alfredo <jose.alfredo@veri.com.br>
2022-09-09 16:23:27 -0300

    Aula 4.2: Migrando Home para CMS

---
+++ src/infra/cms/CMSSectionRender.js
@@ -0,0 +1,17 @@
+import { cmsSections } from "../../components/cmsSections";
+import { getCMSContent } from "./CMSProvider";
+
+export function CMSSectionRender({pageName}){
+
+const sections = getCMSContent(`${pageName}.pageContent[0].section`);
+   return sections.map((sectionProps) => {
+    console.log(sectionProps)
+    const Component = cmsSections[sectionProps.componentName];
+
+        return (
+            
+                 <Component key={sectionProps.id} {...sectionProps}/>
+          
+        ) 
+   })
+}
